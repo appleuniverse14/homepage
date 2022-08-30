a6993e6a5530572c27128d5f8ff77900a20f84d8
taizo122 <pocketspla@gmail.com>
2022-08-30 22:21:43 +0900

    update

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+  res.status(404).render('404', {});
   res.status(err.status || 500);
   res.render('error');
 });
